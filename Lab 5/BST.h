d6c809f319938992c51fc148e7c0bb1f0b02e1a1
Wilson <wilson.wu.002@gmail.com>
2020-06-07 23:44:45 -0700

    created BST class

---
+++ Lab 5/BST.h
@@ -0,0 +1,17 @@
+//
+//  Header.h
+//  Lab 5
+//
+//  Created by Wilson Wu on 6/7/20.
+//  Copyright © 2020 None. All rights reserved.
+//
+
+#ifndef BST_h
+#define BST_h
+
+template <typename T>
+class BST
+{
+    
+};
+#endif /* Header_h */
