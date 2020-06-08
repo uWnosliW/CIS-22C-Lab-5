418dec43fc0f87aaaf9396afef22d36b40b750d3
Wilson <wilson.wu.002@gmail.com>
2020-06-07 23:27:51 -0700

    init

---
+++ Lab 5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab 5
+//
+//  Created by Wilson Wu on 6/7/20.
+//  Copyright © 2020 None. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
